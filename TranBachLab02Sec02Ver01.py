391e2a5119c2c2efce5b43c81c9a390a44d1078b
nhubach5998 <nhubach5998@gmail.com>
2017-01-13 15:24:01 -0800

    Update TranBachLab02Sec02Ver01.py

---
+++ TranBachLab02Sec02Ver01.py
@@ -0,0 +1,7 @@
+#Create a program that uses a loop to print out the numbers 0-9.
+print("This is series from 0 to 9")
+for i in range(10):
+    print(i,end="\t")
+#Then add another loop that prints out the numbers 1-10.
+print("\nThis is series from 1 to 10")
+#Then add some more code to print out the numbers 0-9 in one column and the numbers 10-19 in a column next to it
