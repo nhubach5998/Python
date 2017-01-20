113fa6395a752144ee9eba4a7ab1154392184dd9
nhubach5998 <nhubach5998@gmail.com>
2017-01-20 10:36:59 -0800

    Update

--- TranBachLab03SecHY2Ver01.py
+++ TranBachLab03SecHY2Ver01.py
@@ -1,3 +1,19 @@
+# Project:      
+# Name:         Bach Tran
+# Date:         
+# Description:  
+# 
+# 
 #10.50 a pound + shipping
 #shipping = 0.86/lb + 1.5
 #enter pound and get price
+def CostOfCoffee():
+    fltPounds = float(input("Enter the desired amount of coffee (in lbs): "))
+    fltCost = 10.50 * fltPounds
+    fltShippingCost = 0.86 * fltPounds + 1.50
+    fltTotal = fltCost + fltShippingCost
+    print("""Subtotal : ${0:0.2f}
+    Shipping : ${1:0.2f}
+    Total : ${2:<}""".format(fltCost,fltShippingCost,fltTotal))
+
+CostOfCoffee()
